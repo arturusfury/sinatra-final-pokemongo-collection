cb5e0481f1fba314859c651d4e57b8898e8ffb97
arturusfury <bri.reynolds@gmail.com>
2016-08-01 09:44:33 -0500

    removed extra json parse that wasn't needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,6 @@ until pokedex_num > 151 do
   pokemon = HTTParty.get("http://pokeapi.co/api/v1/pokemon/#{pokedex_num}")
   parsed_pokemon_data = JSON.parse(pokemon.body)
 
-  sprite_request_url = parsed_pokemon_data['sprites'].first['resource_uri']
-  sprite = HTTParty.get("http://pokeapi.co#{sprite_request_url}")
-  parsed_sprite_data = JSON.parse(sprite.body)
-
   @name = parsed_pokemon_data['name']
   @id = parsed_pokemon_data['national_id']
 
