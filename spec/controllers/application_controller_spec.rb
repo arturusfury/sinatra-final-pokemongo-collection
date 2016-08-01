a1e89828085af5e2d8eb9674612a47ec1253595c
arturusfury <bri.reynolds@gmail.com>
2016-08-01 18:50:50 -0500

    added a test for the main page redirect if logged in

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -8,5 +8,18 @@ describe ApplicationController do
       expect(last_response.status).to eq(200)
       expect(last_response.body).to include('The <strong>PokemonGo</strong> Collection')
     end
+
+    it 'will redirect to the users collection if already logged in' do
+      User.create(username: 'skittles123', email: 'skittles@aol.com', password: 'rainbows')
+      params = {
+        inputUsername: 'skittles123',
+        inputPassword: 'rainbows'
+      }
+      post '/login', params
+      expect(last_response.status).to eq(302)
+      follow_redirect!
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include('Current <strong>Collection</strong>')
+    end
   end
 end
