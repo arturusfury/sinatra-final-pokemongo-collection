c072584ffe7301717e31d842d0fcb512cd31d32e
arturusfury <bri.reynolds@gmail.com>
2016-08-01 16:47:24 -0500

    updated login and user creation boxes and fixed error in form code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,31 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/collection' do
+    erb :'users/show'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:inputUsername])
+    if user && user.authenticate(params[:inputPassword])
+      session[:user_id] = user.id
+      redirect to '/collection'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/new' do
+    if params[:inputUsername] == '' || params[:inputEmail] == '' || params[:inputPassword] == ''
+      redirect to '/login'
+    else
+      @user = User.new(username: params[:inputUsername], email: params[:inputEmail], password: params[:inputPassword])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/collection'
+    end
+  end
+
   get '/logout' do
     if logged_in?
       session.destroy
