5fbed90635dec66344c164e3719a803ef8377282
arturusfury <bri.reynolds@gmail.com>
2016-08-01 14:33:59 -0500

    added login and logout paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
+# User Controller Class
 class UsersController < ApplicationController
+  get '/login' do
+    if logged_in?
+      redirect '/'
+    else
+      erb :'users/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
